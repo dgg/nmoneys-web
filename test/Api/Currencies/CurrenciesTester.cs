cdd161812665227e2951b9e606f64884fcbac0e3
Daniel González García <danielgonnet@gmail.com>
2026-01-13 12:46:35 +0100

    test: :white_check_mark: improve error testing

--- test/Api/Currencies/CurrenciesTester.cs
+++ test/Api/Currencies/CurrenciesTester.cs
@@ -5,7 +5,10 @@ using Alba;
 
 using Api.Tests.Currencies.Support;
 
+using FastEndpoints;
+
 using NMoneys;
+using NMoneys.Api.Currencies;
 
 namespace Api.Tests.Currencies;
 
@@ -13,8 +16,8 @@ namespace Api.Tests.Currencies;
 public class CurrenciesTester
 {
 	#region CurrenciesListing
-	
-	[Test , CancelAfter(2000)]
+
+	[Test, CancelAfter(2000)]
 	public async Task CurrenciesListing_ReturnsAllCurrenciesSorted(CancellationToken ct)
 	{
 		var result = await TestWebApp.Host.Scenario(x =>
@@ -23,7 +26,7 @@ public class CurrenciesTester
 			x.StatusCodeShouldBeOk();
 		});
 		using var response = await result.ReadAsJsonAsync<JsonDocument>();
-		
+
 		JsonElement currencies = assertTopLevelCurrencies(response);
 		assertAllCurrencies(currencies);
 		assertSortedCurrencies(currencies);
@@ -34,7 +37,7 @@ public class CurrenciesTester
 	{
 		Assert.That(response, Is.Not.Null);
 
-		Assert.That(response.RootElement.ValueKind, Is.EqualTo(JsonValueKind.Object), 
+		Assert.That(response.RootElement.ValueKind, Is.EqualTo(JsonValueKind.Object),
 			"top-level object");
 		Assert.That(response.RootElement.TryGetProperty("currencies", out var currencies), Is.True);
 		Assert.That(currencies.ValueKind, Is.EqualTo(JsonValueKind.Array), "currencies[]");
@@ -55,18 +58,18 @@ public class CurrenciesTester
 		int currencyCount = Currency.FindAll().Count();
 		Assert.That(currencies.GetArrayLength(), Is.EqualTo(currencyCount), "all currencies");
 	}
-	
+
 	private static void assertSnapshotProps(JsonElement snapshot)
 	{
 		Assert.That(snapshot, Haz.Prop("alphabetic_code"));
 		Assert.That(snapshot, Haz.Prop("english_name"));
 	}
-	
+
 	#endregion
-	
+
 	#region CurrencyRetrieval
-	
-	[Test , CancelAfter(2000)]
+
+	[Test, CancelAfter(2000)]
 	public async Task CurrencyRetrieval_InvalidCode_BadRequest(CancellationToken ct)
 	{
 		string nonAnIsoCode = "LOL";
@@ -75,12 +78,23 @@ public class CurrenciesTester
 			x.Get.Url($"/currencies/{nonAnIsoCode}");
 			x.StatusCodeShouldBe(HttpStatusCode.BadRequest);
 		});
-		using var response = await result.ReadAsJsonAsync<JsonDocument>();
-		Assert.That(response, Is.Not.Null);
-		// TODO: assert problem
+		var problem = await result.ReadAsJsonAsync<ProblemDetails>();
+		Assert.That(problem, Is.Not.Null);
+		Assert.Multiple(() =>
+		{
+			Assert.That(problem.Status, Is.EqualTo(400));
+			Assert.That(problem.Title, Is.EqualTo("Bad Request"));
+			Assert.That(problem.Detail, Does.Contain(nonAnIsoCode).And
+				.Contains(nameof(CurrencyIsoCode)));
+			var errors = problem.Errors.ToArray();
+			Assert.That(errors, Has.Length.EqualTo(1));
+			Assert.That(errors[0].Name, Is.EqualTo("alphabetic_code"));
+			Assert.That(errors[0].Reason, Does.Contain(nonAnIsoCode).And
+				.Contains(nameof(CurrencyIsoCode)));
+		});
 	}
-	
-	[Test , CancelAfter(2000)]
+
+	[Test, CancelAfter(2000)]
 	public async Task CurrencyRetrieval_ValidCode_Details(CancellationToken ct)
 	{
 		string isoCode = "DKK";
@@ -96,8 +110,8 @@ public class CurrenciesTester
 		assertName(currency);
 		assertDetailProps(currency);
 	}
-	
-	[Test , CancelAfter(2000)]
+
+	[Test, CancelAfter(2000)]
 	public async Task CurrencyRetrieval_CaseInsensitive_Details(CancellationToken ct)
 	{
 		string isoCode = "dkk";
@@ -110,12 +124,12 @@ public class CurrenciesTester
 		Assert.That(response, Is.Not.Null);
 		assertTopLevelCurrency(response);
 	}
-	
+
 	private static JsonElement assertTopLevelCurrency(JsonDocument response)
 	{
 		Assert.That(response, Is.Not.Null);
 
-		Assert.That(response.RootElement.ValueKind, Is.EqualTo(JsonValueKind.Object), 
+		Assert.That(response.RootElement.ValueKind, Is.EqualTo(JsonValueKind.Object),
 			"top-level object");
 		Assert.That(response.RootElement.TryGetProperty("currency", out var currency), Is.True);
 		Assert.That(currency.ValueKind, Is.EqualTo(JsonValueKind.Object), "currency{}");
@@ -130,7 +144,7 @@ public class CurrenciesTester
 		Assert.That(codes, Haz.Prop("numeric"));
 		Assert.That(codes, Haz.Prop("padded"));
 	}
-	
+
 	private static void assertName(JsonElement currency)
 	{
 		var names = currency.GetProperty("names");
@@ -138,22 +152,22 @@ public class CurrenciesTester
 		Assert.That(names, Haz.Prop("english"));
 		Assert.That(names, Haz.Prop("native"));
 	}
-	
+
 	private static void assertDetailProps(JsonElement currency)
 	{
 		Assert.That(currency, Haz.Prop("symbol"));
 		Assert.That(currency, Haz.Prop("significant_digits"));
 		Assert.That(currency, Haz.Prop("is_obsolete"));
 	}
-	
+
 	#endregion
-	
+
 	#region AmountsFormatting
-	
+
 	[Test, CancelAfter(2000)]
 	public async Task AmountsFormatting_CodeAmounts_Success(CancellationToken ct)
 	{
-		string alphabeticCode = "DKK"; 
+		string alphabeticCode = "DKK";
 		var result = await TestWebApp.Host.Scenario(x =>
 		{
 			x.Post.Json(new { amounts = new[] { -123.45m, 999.999m } }).ToUrl($"/currencies/{alphabeticCode}/formats");
@@ -166,11 +180,11 @@ public class CurrenciesTester
 		// comma-separated decimals and round up to 2 decimals
 		assertFormatted(formatted, ["-123,45 kr.", "1.000,00 kr."]);
 	}
-	
+
 	[Test, CancelAfter(2000)]
 	public async Task AmountsFormatting_SingleAmount_Success(CancellationToken ct)
 	{
-		string alphabeticCode = "USD"; 
+		string alphabeticCode = "USD";
 		var result = await TestWebApp.Host.Scenario(x =>
 		{
 			x.Post.Json(new { amounts = new[] { -123.45m } }).ToUrl($"/currencies/{alphabeticCode}/formats");
@@ -183,35 +197,56 @@ public class CurrenciesTester
 		// dot-separated decimals
 		assertFormatted(formatted, ["-$123.45"]);
 	}
-	
-	[Test , CancelAfter(2000)]
+
+	[Test, CancelAfter(2000)]
 	public async Task AmountsFormatting_InvalidCode_BadRequest(CancellationToken ct)
 	{
-		string nonAnIsoCode = "LOL";
+		string nonIsoCode = "LOL";
 		var result = await TestWebApp.Host.Scenario(x =>
 		{
-			x.Post.Json(new []{0m}, JsonStyle.MinimalApi).ToUrl($"/currencies/{nonAnIsoCode}/formats");
+			x.Post.Json(new[] { 0m }, JsonStyle.MinimalApi).ToUrl($"/currencies/{nonIsoCode}/formats");
 			x.StatusCodeShouldBe(HttpStatusCode.BadRequest);
 		});
-		using var response = await result.ReadAsJsonAsync<JsonDocument>();
-		Assert.That(response, Is.Not.Null);
-		// TODO: assert problem
+
+		var problem = await result.ReadAsJsonAsync<ProblemDetails>();
+		Assert.That(problem, Is.Not.Null);
+		Assert.Multiple(() =>
+		{
+			Assert.That(problem.Status, Is.EqualTo(400));
+			Assert.That(problem.Title, Is.EqualTo("Bad Request"));
+			Assert.That(problem.Detail, Does.Contain(nameof(AmountsFormattingRequest)),
+				"different message");
+			var errors = problem.Errors.ToArray();
+			Assert.That(errors, Has.Length.EqualTo(1));
+			Assert.That(errors[0].Name, Is.EqualTo("serializer_errors"), "not attached to property");
+			Assert.That(errors[0].Reason, Does.Contain(nameof(AmountsFormattingRequest)));
+		});
 	}
-	
-	[Test , CancelAfter(2000)]
-	public async Task AmountsFormatting_CaseInsensitiveCurrency(CancellationToken ct)
+
+	[Test, CancelAfter(2000)]
+	public async Task AmountsFormatting_WrongCasing_BadRequest(CancellationToken ct)
 	{
 		string alphabeticCode = "dkk";
 		var result = await TestWebApp.Host.Scenario(x =>
 		{
-			x.Post.Json(new []{0m}, JsonStyle.MinimalApi).ToUrl($"/currencies/{alphabeticCode}/formats");
+			x.Post.Json(new[] { 0m }, JsonStyle.MinimalApi).ToUrl($"/currencies/{alphabeticCode}/formats");
 			x.StatusCodeShouldBe(HttpStatusCode.BadRequest);
 		});
-		using var response = await result.ReadAsJsonAsync<JsonDocument>();
-		Assert.That(response, Is.Not.Null);
-		// TODO: assert currency
+		var problem = await result.ReadAsJsonAsync<ProblemDetails>();
+		Assert.That(problem, Is.Not.Null);
+		Assert.Multiple(() =>
+		{
+			Assert.That(problem.Status, Is.EqualTo(400));
+			Assert.That(problem.Title, Is.EqualTo("Bad Request"));
+			Assert.That(problem.Detail, Does.Contain(nameof(AmountsFormattingRequest)),
+				"different message");
+			var errors = problem.Errors.ToArray();
+			Assert.That(errors, Has.Length.EqualTo(1));
+			Assert.That(errors[0].Name, Is.EqualTo("serializer_errors"), "not attached to property");
+			Assert.That(errors[0].Reason, Does.Contain(nameof(AmountsFormattingRequest)));
+		});
 	}
-	
+
 	[Test, CancelAfter(2000)]
 	public async Task AmountsFormatting_EmptyAmounts_BadRequest(CancellationToken ct)
 	{
@@ -220,16 +255,25 @@ public class CurrenciesTester
 			x.Post.Json(new { amounts = Array.Empty<decimal>() }).ToUrl("/currencies/CHF/formats");
 			x.StatusCodeShouldBe(HttpStatusCode.BadRequest);
 		});
-		using var response = await result.ReadAsJsonAsync<JsonDocument>();
-		Assert.That(response, Is.Not.Null);
-		// TODO: assert problem
+		var problem = await result.ReadAsJsonAsync<ProblemDetails>();
+		Assert.That(problem, Is.Not.Null);
+		Assert.Multiple(() =>
+		{
+			Assert.That(problem.Status, Is.EqualTo(400));
+			Assert.That(problem.Title, Is.EqualTo("Bad Request"));
+			Assert.That(problem.Detail, Does.Contain("amounts").And.Contain("empty"));
+			var errors = problem.Errors.ToArray();
+			Assert.That(errors, Has.Length.EqualTo(1));
+			Assert.That(errors[0].Name, Is.EqualTo("amounts"), "not attached to property");
+			Assert.That(errors[0].Reason, Does.Contain("amounts").And.Contain("empty"));
+		});
 	}
-	
+
 	private static JsonElement assertTopLevelFormatted(JsonDocument response)
 	{
 		Assert.That(response, Is.Not.Null);
 
-		Assert.That(response.RootElement.ValueKind, Is.EqualTo(JsonValueKind.Object), 
+		Assert.That(response.RootElement.ValueKind, Is.EqualTo(JsonValueKind.Object),
 			"top-level object");
 		Assert.That(response.RootElement.TryGetProperty("formatted", out var formatted), Is.True);
 		Assert.That(formatted.ValueKind, Is.EqualTo(JsonValueKind.Array), "formatted{}");
@@ -247,6 +291,6 @@ public class CurrenciesTester
 			}
 		});
 	}
-	
+
 	#endregion
 }
